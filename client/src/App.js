2b91d8bc6c3074a2072ac7e80a14c1fdecf0b489
BiratBC <104758456+BiratBC@users.noreply.github.com>
2025-01-12 19:31:33 +0545

    Front end start

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <>
-    <h1>Birat</h1>
-    
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+        }}
+      >
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
